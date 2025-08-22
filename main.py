858252421e94376cfe58dbfb6e6dd05cb5b98640
Maria Del Rosario Castro <71073724+mariadelrosario98@users.noreply.github.com>
2025-08-21 20:31:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["png", "jpg", "jpeg
 if uploaded_file is not None:
     # Display the uploaded image
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
     st.markdown("---")
 
     # OCR Processing
